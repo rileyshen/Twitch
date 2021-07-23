4a67976a7160ba9bb28292a3200f3de6db92c332
RileyCode <ripple.shen31@gmail.com>
2021-07-23 23:12:50 +0800

    Game entiry

---
+++ src/main/java/com/laioffer/jupiter/entiry/Game.java
@@ -0,0 +1,95 @@
+package com.laioffer.jupiter.entiry;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Locale;
+
+/**
+ * @Description:
+ * @Author RileyShen
+ * @Create 2021-07-21
+ */
+
+public class Game {
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("developer")
+    private String developer;
+
+    @JsonProperty("releaseTime")
+    private String releaseTime;
+
+    @JsonProperty("website")
+    private String website;
+
+    @JsonProperty("price")
+    private double price;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public String getReleaseTime() {
+        return releaseTime;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Game(Builder builder) {
+        this.name = builder.name;
+        this.developer = builder.developer;
+        this.releaseTime = builder.releaseTime;
+        this.website = builder.website;
+        this.price = builder.price;
+
+    }
+
+    public static class Builder {
+        private String name;
+        private String developer;
+        private String releaseTime;
+        private String website;
+        private double price;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setDeveloper(String developer) {
+            this.developer = developer;
+            return this;
+        }
+
+        public Builder setReleaseTime(String releaseTime) {
+            this.releaseTime = releaseTime;
+            return this;
+        }
+
+        public Builder setWebsite(String website) {
+            this.website = website;
+            return this;
+        }
+
+        public Builder setPrice(double price) {
+            this.price = price;
+            return this;
+        }
+
+        public Game build() {
+            return new Game(this);
+        }
+    }
+}
--- src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
+++ src/main/java/com/laioffer/jupiter/servlet/GameServlet.java
@@ -7,13 +7,58 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "GameServlet", value = "/game")
-public class GameServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
 
+import com.laioffer.jupiter.entiry.Game;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebServlet(name = "GameServlet", urlPatterns = {"/game"})
+public class GameServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Read game information from request body
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(req.getReader()));
+        String name = jsonRequest.getString("name");
+        String developer = jsonRequest.getString("developer");
+        String releaseTime = jsonRequest.getString("release_time");
+        String website = jsonRequest.getString("website");
+        float price = jsonRequest.getFloat("price");
+        // Print game information to IDE console
+        System.out.println("Name is: " + name);
+        System.out.println("Developer is: " + developer);
+        System.out.println("Release time is: " + releaseTime);
+        System.out.println("Website is: " + website);
+        System.out.println("Price is: " + price);
+        // Return status = ok as response body to the client
+        resp.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        resp.getWriter().print(jsonResponse);
     }
 
+
+
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("Hello World");
+
+        response.setContentType("application/json");
+        ObjectMapper mapper = new ObjectMapper();
+
+        Game.Builder builder = new Game.Builder();
+        builder.setName("World of Warcraft");
+        builder.setDeveloper("Blizzard Entertainment");
+        builder.setReleaseTime("Feb 11, 2005");
+        builder.setWebsite("https://www.worldofwarcraft.com");
+        builder.setPrice(49.99);
+
+        Game game = builder.build();
+
+
+        // Write game information to response body
+        response.getWriter().print(mapper.writeValueAsString(game));
+
+
     }
 }
